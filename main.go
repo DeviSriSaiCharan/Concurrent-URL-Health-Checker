04d7c30e263203b5e53b3cc72a03cb9d0bbf2a33
DeviSriSaiCharan <devisrisaicharan@gmail.com>
2026-06-13 12:21:44 +0530

    feat: add result printing functionality with pagination and color coding

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/fatih/color"
+
 	"github.com/DeviSriSaiCharan/GoLang-Learnings/checker"
+	"github.com/olekukonko/tablewriter"
+	"golang.org/x/term"
 )
 
 var client http.Client = http.Client{
@@ -63,6 +69,8 @@ func main() {
 	close(results)
 
 	fmt.Printf("Total time to check health for %d urls: %v\n", len(results), healthCheckerEndTime)
+
+	printResults(results)
 }
 
 func getUrlsFromTextFile(filePath string) ([]string, error) {
@@ -102,4 +110,115 @@ func worker(id int, jobs <-chan string, results chan<- checker.HealthResult, wg 
 		results <- result
 	}
 
+}
+
+func printResults(results <-chan checker.HealthResult) {
+	i := 1
+	data := [][]string{}
+
+	for result := range results {
+		status := strconv.Itoa(result.StatusCode)
+		var coloredStatus string
+
+		switch {
+		case result.StatusCode >= 200 && result.StatusCode < 300:
+			coloredStatus = color.New(color.FgGreen, color.Bold).Sprint(status)
+
+		case result.StatusCode >= 300 && result.StatusCode < 400:
+			coloredStatus = color.New(color.FgYellow, color.Bold).Sprint(status)
+
+		default:
+			coloredStatus = color.New(color.FgRed, color.Bold).Sprint(status)
+		}
+
+		errorMessage := ""
+
+		if result.Err != nil {
+			errorMessage = result.Err.Error()
+		}
+
+		outputResult := []string{
+			strconv.Itoa(i),
+			result.Url,
+			coloredStatus,
+			errorMessage,
+		}
+
+		data = append(data, outputResult)
+		i++
+	}
+
+	_, height, err := term.GetSize(int(os.Stdout.Fd()))
+
+	if err != nil {
+		fmt.Println("Error getting terminal size")
+		return
+	}
+
+	pageSize := height - 7
+
+	if pageSize <= 0 {
+		pageSize = 5
+	}
+
+	totalData := len(data)
+	totalPages := (totalData + pageSize - 1) / pageSize // Ceil division to get total pages
+	pageToRender := 1
+
+	terminalInputReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("\r\033[H\033[2J")
+
+		startIndex := (pageToRender - 1) * pageSize
+		endIndex := startIndex + pageSize
+
+		if endIndex > totalData {
+			endIndex = totalData
+		}
+
+		pageData := data[startIndex:endIndex]
+
+		table := tablewriter.NewTable(os.Stdout)
+		table.Header(
+			"#",
+			"URL",
+			"Status Code",
+			"Error Message",
+		)
+		table.Bulk(pageData)
+		table.Render()
+
+		fmt.Printf("\nShowing %d-%d of %d.\n", startIndex+1, endIndex, totalData)
+
+		if totalPages > 1 {
+			if pageToRender == 1 {
+				fmt.Print("[n] Next | [q] Quit: ")
+			} else if pageToRender == totalPages {
+				fmt.Print("[p] Previous | [q] Quit: ")
+			} else {
+				fmt.Print("[p] Previous | [n] Next | [q] Quit: ")
+			}
+		} else {
+			fmt.Print("[q] Quit: ")
+		}
+
+		input, _ := terminalInputReader.ReadString('\n')
+		input = strings.TrimSpace(strings.ToLower(input))
+
+		switch input {
+		case "n":
+			if pageToRender < totalPages {
+				pageToRender++
+			}
+		case "p":
+			if pageToRender > 1 {
+				pageToRender--
+			}
+		case "q":
+			return
+		default:
+			fmt.Println("Invalid input. Please enter 'n', 'p', or 'q'.")
+		}
+	}
 }
